087985c8450891c5572b9f11724557758222db4a
Yassine Abdelghafour <yabdelgh@e2r8p7.1337.ma>
2022-01-10 13:46:03 +0100

    added operator[]

--- main.cpp
+++ main.cpp
@@ -6,16 +6,9 @@ int main()
 {
 	ft::vector<int> name1(1999);
 	std::vector<int> name2(1999);
-	
-	std::cout << name1.size() << std::endl;
-	std::cout << name1.max_size() << std::endl;
-	std::cout << name1.capacity() << std::endl;
-	std::cout << name1.empty() << std::endl;
-	std::cout << "||||||||||||||||||||||||||||||||||" << std::endl;
-	std::cout << name2.size() << std::endl;
-	std::cout << name2.max_size() << std::endl;
-	std::cout << name2.capacity() << std::endl;
-	std::cout << name2.empty() << std::endl;
+
+	std::cout << name1[9999999] << std::endl;
+//	std::cout << name2[9999999] << std::endl;
 	
 	return (0);
 }
--- vector.hpp
+++ vector.hpp
@@ -4,7 +4,6 @@
 
 namespace ft
 {
-	
 	template < class T, class Allocator = std::allocator<T> >
 	class vector
 	{
@@ -15,10 +14,10 @@ namespace ft
 		typedef typename allocator_type::const_reference const_reference;
 		typedef typename allocator_type::pointer         pointer;
 		typedef typename allocator_type::const_pointer   const_pointer;
-	//	typedef implementation-defined                   iterator;
-	//	typedef implementation-defined                   const_iterator;
-	//	typedef std::reverse_iterator<iterator>          reverse_iterator;
-	//	typedef std::reverse_iterator<const_iterator>    const_reverse_iterator;
+		//	typedef implementation-defined                   iterator;
+		//	typedef implementation-defined                   const_iterator;
+		//	typedef std::reverse_iterator<iterator>          reverse_iterator;
+		//	typedef std::reverse_iterator<const_iterator>    const_reverse_iterator;
 		typedef typename allocator_type::difference_type difference_type;
 		typedef typename allocator_type::size_type       size_type;
 
@@ -36,7 +35,7 @@ namespace ft
 			_data = A.allocate(n);
 		}
 		//template <class InputIterator>
-        //vector (InputIterator first, InputIterator last,
+		//vector (InputIterator first, InputIterator last,
 		//		const allocator_type& allocator = allocator_type());
 		//vector (const vector& x);
 
@@ -58,7 +57,16 @@ namespace ft
 		{
 			return (!_size);
 		}
+		
 		public: // Element access
+		reference operator[] (size_type n)
+		{
+			return (_data[n]);
+		}
+		const_reference operator[] (size_type n) const
+		{
+				return (_data[n]);
+		}
 		public: // Modifiers
 		public: // Allocator
 		public: // Non-member function overloads
@@ -73,9 +81,6 @@ namespace ft
 		{
 		}
 	};
-
-
-	
 }
 
 #endif
