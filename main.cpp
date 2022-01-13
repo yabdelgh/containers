225ab03d272cca0d5ab9c438db5ffbf64d1fad18
yabdelgh <yassine.abdou.1998@gmail.com>
2022-01-13 23:09:38 +0100

    moved all member functions outside the class

--- main.cpp
+++ main.cpp
@@ -4,5 +4,18 @@
 
 int main()
 {
+	ft::vector<int> my(10);
+
+	std::cout << my.front() << std::endl;
+	std::cout << my.back() << std::endl;
+	std::cout << my[9] << std::endl;
+	try
+	{
+		std::cout << my.at(66666) << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	return (0);
 }
--- vector.hpp
+++ vector.hpp
@@ -22,18 +22,8 @@ namespace ft
 		typedef typename allocator_type::size_type       size_type;
 
 		public: // constructors
-		explicit vector (const allocator_type& allocator = allocator_type())
-		{
-			_size = 0;
-			_capacity = 0;
-			_data = nullptr;
-		}
-		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& allocator = allocator_type())
-		{
-			_size = n;
-			_capacity = n;
-			_data = A.allocate(n);
-		}
+		explicit vector (const allocator_type& allocator = allocator_type());
+		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& allocator = allocator_type());
 		//template <class InputIterator>
 		//vector (InputIterator first, InputIterator last,
 		//		const allocator_type& allocator = allocator_type());
@@ -41,61 +31,21 @@ namespace ft
 
 		public: // Iterators
 		public: // Capacity
-		size_type size(void) const
-		{
-			return (_size);
-		}
-		size_type max_size(void) const
-		{
-			return (A.max_size());
-		}
-		size_type capacity(void) const
-		{
-			return (_capacity);
-		}
-		bool empty() const
-		{
-			return (!_size);
-		}
+		size_type size(void) const;
+		size_type max_size(void) const;
+		size_type capacity(void) const;
+		bool empty() const;
 		
 		public: // Element access
 
-		reference operator[] (size_type n)
-		{
-			return (_data[n]);
-		}
-		const_reference operator[] (size_type n) const
-		{
-				return (_data[n]);
-		}
-		reference at (size_type n)
-		{
-			if (n >= _size)
-				throw std::out_of_range("vector::at");
-			return (_data[n]);
-		}
-		const_reference at (size_type n) const
-		{
-			if (n >= _size)
-				throw std::out_of_range("vector::at");
-			return (_data[n]);
-		}
-		reference front()
-		{
-			return (_data[0]);
-		}
-		const_reference front() const
-		{
-			return (_data[0]);
-		}
-		reference back()
-		{
-			return (_data[_size - 1]);
-		}
-		const_reference back() const
-		{
-			return (_data[_size - 1]);
-		}
+		reference operator[] (size_type n);
+		const_reference operator[] (size_type n) const;
+		reference at (size_type n);
+		const_reference at (size_type n) const;
+		reference front();
+		const_reference front() const;
+		reference back();
+		const_reference back() const;
 
 		public: // Modifiers
 		public: // Allocator
@@ -111,6 +61,113 @@ namespace ft
 		{
 		}
 	};
+	
+	// Constructors
+	template < class T, class Allocator >
+	vector<T, Allocator>::vector (const allocator_type& allocator)
+	{
+		_size = 0;
+		_capacity = 0;
+		_data = nullptr;
+	}
+
+	template < class T, class Allocator >
+	vector<T, Allocator>::vector (size_type n, const value_type& val, const allocator_type& allocator)
+	{
+		_size = n;
+		_capacity = n;
+		_data = A.allocate(n);
+	}
+	
+
+	// Capacity
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::size_type
+	vector<T, Allocator>::size(void) const
+	{
+		return (_size);
+	}
+
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::size_type
+	vector<T, Allocator>::max_size(void) const
+	{
+		return (A.max_size());
+	}
+
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::size_type
+	vector<T, Allocator>::capacity(void) const
+	{
+		return (_capacity);
+	}
+
+	template < class T, class Allocator >
+	bool vector<T, Allocator>::empty() const
+	{
+		return (!_size);
+	}
+	
+	// Element access	
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::reference
+	vector<T, Allocator>::operator[](size_type n)
+	{
+		return (_data[n]);
+	}
+
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::const_reference
+	vector<T, Allocator>::operator[](size_type n) const
+	{
+		return (_data[n]);
+	}
+
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::reference
+	vector<T, Allocator>::at(size_type n)
+	{
+		if (n >= _size)
+			throw std::out_of_range("vector::at");
+		return (_data[n]);
+	}
+	
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::const_reference
+	vector<T, Allocator>::at(size_type n) const
+	{
+		if (n >= _size)
+			throw std::out_of_range("vector::at");
+		return (_data[n]);
+	}
+	
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::reference
+	vector<T, Allocator>::front()
+	{
+			return (_data[0]);
+	}
+	
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::const_reference
+	vector<T, Allocator>::front() const
+	{
+			return (_data[0]);
+	}
+	
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::const_reference
+	vector<T, Allocator>::back() const
+	{
+			return (_data[_size - 1]);
+	}
+	
+	template < class T, class Allocator >
+	typename vector<T, Allocator>::reference
+	vector<T, Allocator>::back()
+	{
+			return (_data[_size - 1]);
+	}
 }
 
 #endif
